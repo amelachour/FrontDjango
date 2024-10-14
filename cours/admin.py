de1bf4809d2622deb4abf9968806547af89e9c73
Amel Achour <amelacho3@gmail.com>
2024-10-14 23:40:24 +0100

    Affichage Courses et détailées

--- cours/admin.py
+++ cours/admin.py
@@ -1,2 +1,6 @@
-from django.contrib import admin
+# admin.py
 
+from django.contrib import admin
+from .models import Cours
+
+admin.site.register(Cours)
--- cours/forms.py
+++ cours/forms.py
@@ -6,16 +6,4 @@ class CourseForm(forms.ModelForm):
         model = Cours
         fields = ['title', 'description', 'file']
     
-    def __init__(self, *args, **kwargs):
-        super(CourseForm, self).__init__(*args, **kwargs)
-        self.fields['title'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'Entrez le titre du cours'
-        })
-        self.fields['description'].widget.attrs.update({
-            'class': 'form-control',
-            'placeholder': 'Entrez une description du cours'
-        })
-        self.fields['file'].widget.attrs.update({
-            'class': 'form-control'
-        })
+   
